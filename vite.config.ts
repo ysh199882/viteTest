e0ea4d1ef0cc00a3d5b459d2542e93212e82954d
yangshihao <yangshihao@bhb.tv>
2022-04-29 11:39:39 +0800

    feat:添加Prettier规范代码格式

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteEslint from 'vite-plugin-eslint';
 import path from 'path'
 import autoprefixer from 'autoprefixer';
 import windi from 'vite-plugin-windicss'
@@ -8,28 +9,38 @@ import eslintPlugin from 'vite-plugin-eslint';
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root:path.join(__dirname,'src'),
-  plugins: [react(),windi(),eslintPlugin()], 
-
-  // css 相关的配置
-  css: {
-    modules:{
-      generateScopedName:"[name]__[local]___[hash:base64:5]"
+export default defineConfig(()=>{
+  root:path.join(__dirname,'src');
+  plugins: [react(),windi(),eslintPlugin(),viteEslint()];
+  return {
+    resolve:{
+      alias:[{find:'@',replacement:'/src'}],
     },
-    preprocessorOptions: {
-      scss: {
-        // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`
+    server:{
+      host:true
+    },
+    preview:{
+      port:3100,
+    },
+      // css 相关的配置
+    css: {
+      modules:{
+        generateScopedName:"[name]__[local]___[hash:base64:5]"
+      },
+      preprocessorOptions: {
+        scss: {
+          // additionalData 的内容会在每个 scss 文件的开头自动注入
+          additionalData: `@import "${variablePath}";`
+        }
+      },
+      postcss:{
+        plugins:[
+          autoprefixer({
+            // 指定目标浏览器
+            overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+          })
+        ]
       }
-    },
-    postcss:{
-      plugins:[
-        autoprefixer({
-          // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
     }
-  }
+  };
 })
